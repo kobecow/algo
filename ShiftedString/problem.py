5a97211fb632698e56fb6cddff98f13f55f00a27
kobecow <sneeiwji@gmail.com>
2019-10-09 00:35:40 -0400

    type: solved
    ShifttedString

--- ShiftedString/problem.py
+++ ShiftedString/problem.py
@@ -6,6 +6,31 @@ Eg. A = abcde, B = cdeab should return true because A can be shifted 3 times to 
 
 def is_shifted(a, b):
   # Fill this in.
+
+  # return True or False
   
-print is_shifted('abcde', 'cdeab')
+
+  if len(a) != len(b):
+    return False
+
+  if a == b:
+    return True
+
+  # checking all index.
+
+  for index in range(len(a)):
+
+    if a[0] == b[index]:
+      left_length = len(a) - index
+
+      if a[:left_length] == b[-left_length:] and a[left_length:] == b[:-left_length]:
+        return True
+
+  return False
+  
+print(is_shifted('abcde', 'cdeab'))
 # True
+
+print(is_shifted('ikaekaj', 'kajikae'))
+
+print(is_shifted('kajikei', 'jkkaie'))
