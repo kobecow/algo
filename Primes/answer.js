0be8452f114f8e5bc4e451a435766e2124ebe9ad
kobecow <sneeiwji@gmail.com>
2020-01-03 14:44:25 -0500

    type: solution
    Cool solution I've never think this way.

---
+++ Primes/answer.js
@@ -0,0 +1,26 @@
+
+// This is not answer for same problem. But consept is cool. There are a lot of learning.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+    if (n <= 1) return 0;// 1 is not a prime number
+
+    var isPrime = [],
+        count = 0;
+
+    for (var i = 2; i < n; i++) {
+        if (isPrime[i] == undefined) { // cannot use !isPrime[i] (isPrime[i] == false) has to be undefined 
+            isPrime[i] = true;
+            count++;
+            for (var j = 2; j * i < n; j++) {
+                // mark the prime's multiples as nonprime
+                isPrime[i * j] = false;
+            }
+        }
+    }
+
+    return count;
+};
