8b248fb31434c79130592fc79d32ace2f66b2028
kobecow <sneeiwji@gmail.com>
2020-01-03 14:17:51 -0500

    type: answer
    add an answer using LLN.
    compare answers (Set vs LLN)
    
    -----execution time--------
    set: 154.395ms
    LL: 56.168ms

--- Primes/problem.js
+++ Primes/problem.js
@@ -46,15 +46,138 @@ const find_primes = n => {
     return Array.from(set_Primes);
 
 }
-// const startTime_Set = performance.now();
+
+/**
+ * 
+ * @param {integer} n 
+ * @return {Array}
+ */
+// Using LinkedList
+
+const find_Primes_LL = n => {
+
+    class Node {
+        constructor(data) {
+            if (data === null) {
+                console.log("Must include data");
+                return Error
+            }
+            this.data = data;
+            this.next = null;
+        }
+
+        getData() {
+            return this.data;
+        }
+    }
+
+    class LinkedList {
+        constructor() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        add(data) {
+            let node = new Node(data);
+
+            let current_node = this.tail;
+
+            if (current_node === null) {
+                this.head = node;
+                this.tail = this.head;
+                return
+            } else {
+                current_node.next = node;
+                this.tail = current_node.next
+                return
+            }
+        }
+
+    }
+
+
+    if (n < 2) {
+        return [];
+    }
+    linkedPrime = new LinkedList();
+    linkedPrime.add(2);
+
+    /**
+     * 
+     * @param {integer} num
+     * @param {Object} LinkedList
+     * 
+     * @return {Bool}
+     */
+    const is_prime = (num, LinkedList) => {
+        let currentNode = LinkedList.head;
+        let currentPrime = currentNode.getData();
+        while (currentPrime !== null) {
+            if (num % currentPrime === 0) {
+                return false;
+            }
+            currentNode = currentNode.next;
+            if (currentNode === null) {
+                return true;
+            }
+            currentPrime = currentNode.getData();
+        }
+    }
+
+    /**
+     * @param {Object} LinkedList
+     * @returns {Array} ans
+     */
+    const LLtoArray = (LinkedList) => {
+        let current_node = LinkedList.head;
+        const ans = [];
+        while (current_node.getData()) {
+            ans.push(current_node.getData());
+            current_node = current_node.next;
+            if (current_node === null) {
+                break;
+            }
+
+
+        }
+        return ans
+
+    }
+
+
+    for (i = 2; i < n; i++) {
+        let flag = is_prime(i, linkedPrime);
+        if (flag) { linkedPrime.add(i) }
+    }
+
+    const ans = LLtoArray(linkedPrime);
+
+    return ans
+}
+
+
+
+
 console.time("set");
 let times_Set = 0;
 while (times_Set < 100) {
-    find_primes(100);
+    find_primes(1000);
     times_Set++
-    console.log(times_Set);
 }
 console.timeEnd("set");
-// const endTime_Set = performance.now();
 
-// console.log(endTime_Set - startTime_Set);
+console.time("LL");
+let times_LL = 0;
+while (times_LL < 100) {
+    find_Primes_LL(1000);
+    times_LL++
+}
+console.timeEnd("LL");
+
+
+
+// TODO: Should compare array.
+
+
+const ll = find_Primes_LL(100);
+const ss = find_primes(100);
