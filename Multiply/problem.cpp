79724751fbc4959868e910ea9b4a70b3f29405e8
kobecow <sneeiwji@gmail.com>
2020-01-06 22:55:10 -0500

    type: problem
    still thinking overview

--- Multiply/problem.cpp
+++ Multiply/problem.cpp
@@ -2,4 +2,17 @@
 Given two strings which represent non-negative integers,
 multiply the two numbers and return the product as a string as well.
 You should assume that the numbers may be sufficiently large such that the built-in integer type will not be able to store the input
+(Python does have BigNum, but assume it does not exist).
+*/
+
+/**
+ * @param {integer} input1
+ * @param {integer} input2
+ * @return {string} ans (input1 * input2)
+ * 
+ * 1. define max_handable
+ * 2. separate over max_handable and less than handable
+ * 3. ???
+ * 
+ * 
 */
