920d92822931d4096f642c7f5422feaaf96e5443
kobecow <sneeiwji@gmail.com>
2020-01-09 23:09:58 -0500

    type: problem
    handing small input numbers.

--- Multiply/problem.cpp
+++ Multiply/problem.cpp
@@ -27,37 +27,66 @@ int main()
 {
     /*
     three test
-    both x and b is huge number
-    either x or b is huge one
-    neither one is huge
-    */      
-    int x = 177318734; // 0 < x <=2147483647
-    int y = 157138764; // 0 < x <=2147483647
+    both x and b is huge number <-- Done
+    either x or b is huge one <-- Done
+    neither one is huge <-- Done
+
+    ToDo
+    answer is including prefix 0 like 00256. because of small input like 32 * 8.
+    handle it.
+    */
+    int x = 32; // 0 < x <=2147483647
+    int y = 8;  // 0 < x <=2147483647
     // max_handable = 10000 < 46340
     int max_handable = 10000;
-    int x_gt = x / max_handable;
-    //cout << x_gt << endl;
 
-    int x_lt = x - (x_gt * max_handable);
-    //cout << x_lt << endl;
+    int x_gt, x_lt;
 
+    if (x >= max_handable)
+    {
+        x_gt = x / max_handable;
+        //cout << x_gt << endl;
 
-    int y_gt = y / max_handable;
-    //cout << y_gt << endl;
+        x_lt = x - (x_gt * max_handable);
+        //cout << x_lt << endl;
+    }
+    else
+    {
+        x_gt = 0;
+        //cout << x_gt << endl;
 
-    int y_lt = y - (y_gt * max_handable);
-    //cout << y_lt << endl;
+        x_lt = x;
+        //cout << x_lt << endl;
+    }
+
+    int y_gt, y_lt;
+
+    if (y >= max_handable)
+    {
+
+        y_gt = y / max_handable;
+        //cout << y_gt << endl;
+
+        y_lt = y - (y_gt * max_handable);
+        //cout << y_lt << endl;
+    }
+    else
+    {
+        y_gt = 0;
+        //cout << y_gt << endl;
+
+        y_lt = y;
+        //cout << y_lt << endl;
+    }
 
     // left
     int big_xy = x_gt * y_gt;
 
-
     // middle
-    int mid_xy = (x_gt* y_lt) + (x_lt * y_gt);
+    int mid_xy = (x_gt * y_lt) + (x_lt * y_gt);
 
     // right
     int small_xy = x_lt * y_lt;
-
 
     // string-like calculation
     int small_gt = small_xy / max_handable;
@@ -72,11 +101,9 @@ int main()
     // cout << big_sum;
     // cout << mid_lt;
     // cout << small_lt<<endl;
-    string ans =to_string(big_sum) + to_string(mid_lt) + to_string(small_lt);
+    string ans = to_string(big_sum) + to_string(mid_lt) + to_string(small_lt);
 
     cout << ans;
-
-
 
     return 0;
 }
