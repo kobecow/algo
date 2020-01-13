6a0ea37aad364baa160d7f907a84a8a43b591c1c
kobecow <sneeiwji@gmail.com>
2020-01-12 22:20:15 -0500

    type: Docs
    README modified due to typo

--- MultiplyStrings/problem.cpp
+++ MultiplyStrings/problem.cpp
@@ -1,4 +1,28 @@
 /**
  * Given two non-negative integers num1 and num2 represented as strings,
  *  return the product of num1 and num2, also represented as a string.
+ * 
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contain only digits 0-9.
+ * Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
 */
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    string multiply(string num1, string num2)
+    {
+        unsigned int l1 = num1.size(), l2 = num2.size();
+        if (l1 == 0 || l2 == 0)
+            return "0";
+
+        vector<int> v(l1 + l2, 0);
+        vector<int> vv;
+        vector<int> con(11);
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Challenge Algo quiz
-Basically, I try solve quiz to understand algo and data stuctres. Also, I try to learn new languages like **C++, Go**.
+Basically, I try solve quiz to improve understanding of algo and data structure. Also, I try to learn new languages like **C++, Go**.
