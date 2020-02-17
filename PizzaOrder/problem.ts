18cfa99106329749d690a1324b6b08e9f7effbb6
kobecow <sneeiwji@gmail.com>
2020-02-16 22:32:30 -0500

    type: problem
    overview

--- PizzaOrder/problem.ts
+++ PizzaOrder/problem.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
 import * as split from 'split';
+import { exit } from 'process';
 const fsPromises = fs.promises;
 
 class Answer {
@@ -19,18 +20,71 @@ async function solution(filepath: string) {
         input: fs.createReadStream(filepath),
         console: false
     }
+
     /**
      * @param Array <number> arr // sorted ie. [1,3,7,12,20]
      * @param number target
      * @returns number typeNum
      * @returns Array <number> pizzas // sorted ie [1,2,3,6]
      */
-    const main = (arr, target) => {
+    const main = (arr: Array <number>, target: number) => {
+
+        
+        /**
+         * pick one biggest number under target in array
+         * by BinarySearch
+         * 
+         * @param arr of numbers
+         * @param target number
+         * @returns bigNum
+         */
+        const binarySearch = (arr, target) =>{
+            const re_binarySearch = function(arr, target, s_index, e_index) :number{
+                return 1;
+
+            
+            }
+
+            return re_binarySearch(arr, target, 0, arr.length-1);
+
+        }
+
+        /**
+         * pick one biggest number under target in array
+         * 
+         * @param arr of numbers
+         * @param target number
+         * @returns bigNum
+         */
+        const pickBigNum = function(arr: Array<number>, target: number) :number{
+            if(arr[arr.length-1] <= target) return arr.length-1;
+
+            const bigNum = binarySearch(arr, target);
+            return bigNum;
+
+
+
+        }
+
+
         let currentMax :number = 0;
         let wholeMmax : number = 0;
 
-        const binarySearch = (arr, target) =>{
-            
+        let currentNum: number = arr[arr.length -1];
+
+
+        
+        
+        let tmpTarget: number = pickBigNum(arr, target);
+        if(tmpTarget > currentNum*2){
+            let result = main(arr.slice(0,arr.length-1),tmpTarget);
+            if(result){
+                return "Something"
+            }else{
+                return "otherthing"
+            }
+        }else{
+            // binarySearch
         }
     }
 
