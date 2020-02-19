4759ae84b814d655d94622536853f1d3d8cd8d70
kobecow <sneeiwji@gmail.com>
2020-02-19 11:00:44 -0500

    type: problem
    add overview of solution

--- PizzaOrder/problem.ts
+++ PizzaOrder/problem.ts
@@ -36,11 +36,16 @@ async function solution(filepath: string) {
          * 
          * @param arr of numbers
          * @param target number
-         * @returns bigNum
+         * @returns i index // or -1 
          */
         const binarySearch = (arr, target) =>{
             const re_binarySearch = function(arr, target, s_index, e_index) :number{
-                return 1;
+                if(s_index === e_index) return -1;
+                
+                const m_index = Math.floor((s_index + e_index) /2);
+                if(arr[m_index] === target) return m_index;
+                else if(arr[m_index-1] < target && arr[m_index]){}
+
 
             
             }
@@ -54,10 +59,11 @@ async function solution(filepath: string) {
          * 
          * @param arr of numbers
          * @param target number
-         * @returns bigNum
+         * @returns index or -1 
          */
         const pickBigNum = function(arr: Array<number>, target: number) :number{
-            if(arr[arr.length-1] <= target) return arr.length-1;
+            if(arr[arr.length-1] > target) return arr.length-1;
+            if(arr[0] < target) return -1;
 
             const bigNum = binarySearch(arr, target);
             return bigNum;
@@ -68,7 +74,10 @@ async function solution(filepath: string) {
 
 
         let currentMax :number = 0;
+        let currentMaxArr: Array <number> = [];
         let wholeMmax : number = 0;
+        let wholeMmaxArr: Array <number> = [];
+        let isCompleted: boolean = false;
 
         let currentNum: number = arr[arr.length -1];
 
@@ -123,3 +132,59 @@ async function solution(filepath: string) {
 
 }
 solution("./input_files/a_example.in")
+
+
+/*
+Get Biggest one
+again again.
+try small one with last index.
+*/
+
+
+
+
+
+/*
+Pizzas:[2,7,17,23,59,83,142] :240 <= target
+Pizzas:[1,1,1,1,10,10,10,40] :43
+Target:240
+Target:160
+
+40 --> 1 --> 1 --> 1
+
+
+142 --> 17 :159
+142 --> 7 --> 2: 151
+83 --> 59 --> 17 --> :159
+
+142 --> 83 --> 7 -->2 :234 leftOver:6
+142 --> 59 --> 23 --> 7 --> 2 :233 < 240 leftOver:7
+ X 142 --> 59 --> 17 -->  7 --> 2 : < 233 leftOver:7
+142 --> 23 --> 17 -->  7 --> 2 << 240
+
+83 --> 59 --> 23 --> 17 --> 7 --> 2 :191
+ X 59 --> 23 --> 17 --> 7 --> 2: << 191
+leftover: 7
+indexies[6,]
+is_completed:
+
+MaxNumber: 234
+maxIndexies: [6,5,1,0]
+
+
+
+Pizzas:[7,17,23,59,83,101,310,311,,,,,,,,,,,,920,1000] : 1312
+1000 --> 311 X leftOver:1 <-- 
+
+[0, 100]1000 --> [0, 99]310 --> [0,6]
+1000 --> 101 --> 83 --> 59 --> 23 --> 17 --> 7 :1290 = leftOver: 22
+920 --> 
+
+s_index :0
+e_index :100
+m_index:50 --> 101 > 100
+
+
+
+ 
+ */
